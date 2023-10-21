6817a04c156b7df515544c16fff9f037e60cebc4
Vivid Quill <sjdoublec@gmail.com>
2023-10-21 15:51:10 -0500

    First draft of Thought model completed.

--- models/Thought.js
+++ models/Thought.js
@@ -1,3 +1,26 @@
 const { Schema, model } = require('mongoose');
+const { schema } = require('./User');
+
+// Schema to create Thought model:
+const thoughtSchema = new Schema(
+    {
+        thoughtText: { type: String, required: true, minLength: 1, maxLength: 280 },
+        createdAt: { type: Date, default: Date.now },
+        username: { type: String, required: true},
+        reactions: [Reaction] // Array of nested documents created with the 'reactionSchema'
+    }
+)
+
+// Getter method to format the timestamp on query:
+schema.path('createdAt').get(function (timestamp) {
+    return moment(timestamp).format('YYYY-MM-DD');
+});
+
+// Virtual that retrieves the length of the thought's 'reactions' array on query:
+thoughtSchema.virtual('reactionSchema').get(function () {
+    return this.reactions.length;
+});
+
+const Thought = model('Thought', thoughtSchema);
 
 module.exports = Thought;
