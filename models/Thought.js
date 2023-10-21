3263eb58f0e7c9725fcfc4f33af18cba29126dac
Vivid Quill <sjdoublec@gmail.com>
2023-10-21 16:28:03 -0500

    Started work on Reaction model.

--- models/Thought.js
+++ models/Thought.js
@@ -1,20 +1,39 @@
 const { Schema, model } = require('mongoose');
-const { schema } = require('./User');
+const Reaction = require('./Reaction'); // Pull in the 'Reaction' schema for the 'reactions' array.
 
 // Schema to create Thought model:
 const thoughtSchema = new Schema(
     {
-        thoughtText: { type: String, required: true, minLength: 1, maxLength: 280 },
-        createdAt: { type: Date, default: Date.now },
-        username: { type: String, required: true},
+        thoughtText: {
+            type: String,
+            required: true,
+            minLength: 1,
+            maxLength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => moment(createdAtVal).format('YYYY-MM-DD [at] hh:mm a'), // Format date inside
+        },
+        username: {
+            type: String,
+            required: true
+        },
         reactions: [Reaction] // Array of nested documents created with the 'reactionSchema'
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false,
     }
 )
 
 // Getter method to format the timestamp on query:
-schema.path('createdAt').get(function (timestamp) {
-    return moment(timestamp).format('YYYY-MM-DD');
-});
+// schema.path('createdAt').get(function (timestamp) {
+//     return moment(timestamp).format('YYYY-MM-DD');
+// });
 
 // Virtual that retrieves the length of the thought's 'reactions' array on query:
 thoughtSchema.virtual('reactionSchema').get(function () {
