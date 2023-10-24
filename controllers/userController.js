aea303a734a269ab7651a3beed444e0ead8d2c4d
Vivid Quill <sjdoublec@gmail.com>
2023-10-24 11:30:06 -0500

    Delete user route seems to be working.

--- controllers/userController.js
+++ controllers/userController.js
@@ -46,5 +46,17 @@ module.exports = {
             console.log(err);
             res.status(500).json(err);
         }
+    },
+    async deleteUser(req, res) {
+        try {
+            const user = await User.findOneAndRemove({ _id: req.params.userId });
+
+            if (!user) {
+                return res.status(404).json({ message: 'No user with this id!' });
+            }
+            res.json({ message: 'User successfully deleted!' });
+        } catch (err) {
+            res.status(500).json(err);
+        }
     }
 };
