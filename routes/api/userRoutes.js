82f7c152092e32a4a7eb73affa3f3473783c969a
Vivid Quill <sjdoublec@gmail.com>
2023-10-25 11:05:56 -0500

    Seem to have completed all user routes.

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -4,7 +4,9 @@ const {
     getSingleUser,
     createUser,
     updateUser, 
-    deleteUser 
+    deleteUser,
+    addFriend,
+    deleteFriend 
 } = require('../../controllers/userController');
 
 // /api/users
@@ -16,5 +18,19 @@ router
     .get(getSingleUser)
     .put(updateUser)
     .delete(deleteUser);
+
+// /api/users/:userId/friends/:friendId
+
+// Insomnia test code to add Evelyn as a friend to Alice:
+
+// (Below is Alice's "_id" with Evelyn's "_id" afterwards:)
+// POST localhost:3001/api/users/6537e6e4d810c1b204aa9b27/friends/653926a0e40d5c1f6c7a9b0f
+
+// (Below is Evelyn's "_id" in the body of the POST request:)
+// { "_id": "653926a0e40d5c1f6c7a9b0f" }
+router
+    .route('/:userId/friends/:friendId')
+    .post(addFriend) 
+    .delete(deleteFriend); 
 
 module.exports = router;
